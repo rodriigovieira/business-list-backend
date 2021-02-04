0a11458dcc844e566004639abb06bc882963b145
Rodrigo Vieira <rodriigovieira@gmail.com>
2021-02-04 04:07:54 -0300

    Initial commit - setting up project

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
+import 'express-async-errors';
+
+import AppError from './errors/AppError';
+
+import routes from './routes';
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.get('/', (req, res) => {
+  res.status(200).send();
+});
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  console.error(err);
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
+
+app.listen(port, () => console.log(`Server is running at port ${port}!`));
---
+++ src/routes/index.ts
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+
+import businessRoutes from './business.routes';
+
+const routes = Router();
+
+routes.use('/getActiveBusiness', businessRoutes);
+
+export default routes;
